6ef082684f82de910ee4de58a99abb0a5d528a0f
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-14 15:19:15 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-import sys
-print(len(sys.argv) - 1, "argument:")
-x = 1
-while x < len(sys.argv):
-    print("{}: {}".format(x, sys.argv[x]))
-    x += 1
+if __name__ == "__main__":
+    import sys
+    c = len(sys.argv) - 1
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
+        print(c, "argument:")
+    else:
+        print(c, "arguments:")
+    x = 1
+    while x < len(sys.argv):
+        print("{}: {}".format(x, sys.argv[x]))
+        x += 1
