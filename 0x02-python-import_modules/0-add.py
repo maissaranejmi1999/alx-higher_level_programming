93b19d609792d9b3467268ccef6e154076d4bcb1
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-14 06:09:52 -0700

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a, b = 1, 2
-c = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, c))
+from add_0 import add
+a = 1
+b = 2
+c = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, c))
