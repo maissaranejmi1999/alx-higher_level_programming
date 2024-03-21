96032c5105fb56da7779ab12559f8ce61ac4e291
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-20 17:17:12 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    
-    s = float("-inf")
+    s = 0
     for key in a_dictionary:
         if a_dictionary[key] >= s:
             s = a_dictionary[key]
