d6d89b106cdc5961ab35d00fe6018d915a8e3290
maissaranejmi1999 <molinmay1999@gmail.com>
2024-03-20 17:32:26 -0700

    15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
+    dict1 = a_dictionary.copy()
+    for key in dict1:
+        if dict1[key] == value:
             del a_dictionary[key]
     return a_dictionary
