5b09ff7e28cb8f7d9f44015d485164b660b24902
maissaranejmi1999 <molinmay1999@gmail.com>
2024-04-29 13:56:11 -0700

    1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     i = 0
     for i in my_list:
         if i == idx:
-            print("{:d}".format(i))
+            print("Element at index {:d} is {:d}".format(idx, i)
     if i != idx:
         return None
