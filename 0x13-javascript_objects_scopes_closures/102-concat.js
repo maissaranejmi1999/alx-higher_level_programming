bd69ce332d4ed98afc76bca5efd02875d96995d9
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-06-19 14:04:48 -0700

    102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const file3 = process.argv[4];
+
+const data1 = fs.readFileSync(file1, 'utf8');
+const data2 = fs.readFileSync(file2, 'utf8');
+const data3 = data1 + data2;
+
+fs.writeFileSync(file3, data3);
