74394be36dd25e27d29fe4a28a5aacd3a25cbee8
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-06-19 09:52:46 -0700

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 function esrever (list) {
   let len = list.length - 1;
-  const x = len / 2 + 1;
+  const x = len / 2;
   for (let i = 0; i < x; i++) {
     const r = list[i];
     list[i] = list[len];
