2432c283ab70fc0f99153d0f021b9a5408d68dc8
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-07-06 09:20:47 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a class Squar
+"""
+
+
+class Square:
+    """
+    This is a class that defines a square
+    """
+
+    def __init__(self):
+        self.__size = size
