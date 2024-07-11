660013924c68df593049dc1a75188c1ab1c0e9e4
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-07-11 04:25:19 -0700

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,6 +5,10 @@ this module defines a class square
 
 
 class Square:
+    """
+    this is a class that defines a square
+    """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
