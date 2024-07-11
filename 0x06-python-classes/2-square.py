d1bd95e462c56ddaae05bb7c1377070cf686e172
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-07-11 04:10:27 -0700

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+this module defines a class Square
+"""
+
+class Square:
+    """
+    this is a class that defines a Square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
