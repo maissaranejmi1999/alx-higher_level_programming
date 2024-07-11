b3f1c14fb1f444577bed3f9e39f7e26200bd0742
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-07-11 05:09:13 -0700

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,10 +11,11 @@ class Square:
 
     def __init__(self, size=0):
         self.size = size
-
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -23,4 +24,4 @@ class Square:
         self.__size = value
 
     def area(self):
-        return self.size ** 2
+        return self.__size ** 2
