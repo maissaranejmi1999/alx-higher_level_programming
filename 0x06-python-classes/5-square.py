eb32a7f9f85cb9f7827978d016005786b219d7fb
maissaranejmi1999 <maissaranejmi1999@gmail.com>
2024-07-11 05:42:20 -0700

    5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,6 +28,9 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        if self.__size == 0:
+            print()
+            return
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
